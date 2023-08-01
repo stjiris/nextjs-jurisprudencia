2d92ffad83e11bc43369aa55bdbfc7114e599e50
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2023-08-01 11:26:13 +0100

    Random format

--- src/pages/api/boletim/[[...filters]].ts
+++ src/pages/api/boletim/[[...filters]].ts
@@ -13,7 +13,7 @@ export default async function datalistHandler(
     let currentYear = `${date.getFullYear()}`;
     let [area="Área Social", year=currentYear, month=currentMonth, format="pdf"] = Array.isArray(req.query.filters) ? req.query.filters : req.query.filters ? [req.query.filters] : [];
 
-    let pandoc = spawn("pandoc", ["-f","html","-t","pdf","-o","-","--standalone","--pdf-engine","xelatex"], {});
+    let pandoc = spawn("pandoc", ["-f","html","-t",format,"-o","-","--standalone","--pdf-engine","xelatex"], {});
     pandoc.stderr.pipe(process.stderr)
     pandoc.stdout.pipe(res);
     let wls = (...args: string[]) => pandoc.stdin.write(args.join("\n")+"\n");
@@ -22,6 +22,7 @@ export default async function datalistHandler(
         wls(`<head>`)
         wls(`<title>Sumários de Acórdãos - ${area} - ${month}/${year}</title>`)
         wls(`</head>`)
+        wls(`<body>`)
         let r = await client.search<PartialJurisprudenciaDocument>({
             query: {
                 bool: {
@@ -45,11 +46,11 @@ export default async function datalistHandler(
         while( r.hits.hits.length > 0 ){
             for( let hit of r.hits.hits ){
                 if( hit._source?.Descritores?.Show || hit._source?.Descritores?.Original ){
-                    pandoc.stdin.write(`<table><thead>\n`)
+                    pandoc.stdin.write(`<div style="border: 1px solid black">\n`)
                     for( let desc of hit._source?.Descritores.Show || hit._source?.Descritores.Original ){
-                    pandoc.stdin.write(`<tr><th>${desc}</th></tr>\n\n`)
+                    pandoc.stdin.write(`<p><b>${desc}</b></p>\n\n`)
                     }
-                    pandoc.stdin.write(`</thead></table>\n`)
+                    pandoc.stdin.write(`</div>\n`)
                     pandoc.stdin.write(`<div>`);
                     pandoc.stdin.write(hit._source.Sumário || "Sumário não disponível")
                     pandoc.stdin.write(`</div>`)
@@ -60,14 +61,17 @@ export default async function datalistHandler(
             }
             r = await client.scroll({scroll_id: r._scroll_id, scroll: "30s"})
         }
+        wls(`</body>`)
         pandoc.stdin.end()
     }).catch(e => {
         console.error(e);
     })
 
-    res.writeHead(200, {
-        "Content-Type": "application/pdf"
-    })
+    if( format === "pdf" ){
+        res.writeHead(200, {
+            "Content-Type": "application/pdf"
+        })
+    }
     
     return await new Promise(resolve => pandoc.stdout.on("end", resolve))
 }
