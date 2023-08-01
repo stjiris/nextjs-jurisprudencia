cf7cb83ac69a5a48caa682052dcf18b1f1840db9
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2023-08-01 11:58:11 +0100

    Header

--- src/pages/api/boletim/[[...filters]].ts
+++ src/pages/api/boletim/[[...filters]].ts
@@ -12,17 +12,25 @@ export default async function datalistHandler(
     let currentMonth = `${date.getMonth()}`;
     let currentYear = `${date.getFullYear()}`;
     let [area="Área Social", year=currentYear, month=currentMonth, format="pdf"] = Array.isArray(req.query.filters) ? req.query.filters : req.query.filters ? [req.query.filters] : [];
+    let title = `Sumários de Acórdãos - ${area} - ${month}/${year}`
 
-    let pandoc = spawn("pandoc", ["-f","html","-t",format,"-o","-","--standalone","--pdf-engine","xelatex"], {});
+    let pandoc = spawn("pandoc", ["-t",format,"-o","-","--standalone","--pdf-engine","xelatex","--template","pdf-template.tex"], {});
     pandoc.stderr.pipe(process.stderr)
     pandoc.stdout.pipe(res);
     let wls = (...args: string[]) => pandoc.stdin.write(args.join("\n")+"\n");
     getElasticSearchClient().then(async client => {
-        wls(`<!DOCTYPE html>`)
-        wls(`<head>`)
-        wls(`<title>Sumários de Acórdãos - ${area} - ${month}/${year}</title>`)
-        wls(`</head>`)
-        wls(`<body>`)
+        wls(`---`)
+        wls(`title: ${title}`)
+        wls(`output:`)
+        wls(`   beamer_presentation:`)
+        wls(`       keep_tex: true`)
+        wls(`header-includes:`)
+        wls(` - \\usepackage{fancyhdr}`)
+        wls(` - \\pagestyle{fancy}`)
+        wls(` - \\fancyhead{}`)
+        wls(` - \\fancyhead[L]{${title}}`)
+        wls(`---`)
+        wls(``)
         let r = await client.search<PartialJurisprudenciaDocument>({
             query: {
                 bool: {
@@ -45,23 +53,22 @@ export default async function datalistHandler(
         });
         while( r.hits.hits.length > 0 ){
             for( let hit of r.hits.hits ){
+                wls(`<div style="page-break-after: always;">\n`)
                 if( hit._source?.Descritores?.Show || hit._source?.Descritores?.Original ){
-                    pandoc.stdin.write(`<div style="border: 1px solid black">\n`)
-                    for( let desc of hit._source?.Descritores.Show || hit._source?.Descritores.Original ){
-                    pandoc.stdin.write(`<p><b>${desc}</b></p>\n\n`)
-                    }
-                    pandoc.stdin.write(`</div>\n`)
-                    pandoc.stdin.write(`<div>`);
-                    pandoc.stdin.write(hit._source.Sumário || "Sumário não disponível")
-                    pandoc.stdin.write(`</div>`)
-                    pandoc.stdin.write(`<p>${hit._source['Data']}</p>`)
-                    pandoc.stdin.write(`<p>Proc. nº ${hit._source['Número de Processo']}</p>`)
-                    pandoc.stdin.write(`<p>${hit._source['Relator Nome Profissional']?.Show.join("\n")}</p>`)
+                    wls(`---\n`)
+                    wls(...(hit._source?.Descritores.Show || hit._source?.Descritores.Original).map(d => `**${d}**\n`))
+                    wls(`---\n`)
+                    wls(`<div>`);
+                    wls(hit._source.Sumário || "Sumário não disponível")
+                    wls(`</div>`)
+                    wls(`${hit._source['Data']}\n`)
+                    wls(`Proc. nº ${hit._source['Número de Processo']}\n`)
+                    wls(`${hit._source['Relator Nome Profissional']?.Show.join("\n")}\n`)                
                 }
+                wls(`</div>\n`)
             }
             r = await client.scroll({scroll_id: r._scroll_id, scroll: "30s"})
         }
-        wls(`</body>`)
         pandoc.stdin.end()
     }).catch(e => {
         console.error(e);
