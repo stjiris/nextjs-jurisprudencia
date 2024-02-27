d52e7e81dc8be37c81d9de6bc9272f6daee53de2
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2024-02-27 16:09:32 +0000

    Fix related proc

--- src/pages/api/related/[proc]/[uuid].ts
+++ src/pages/api/related/[proc]/[uuid].ts
@@ -5,23 +5,23 @@ import { PartialJurisprudenciaDocument } from '@stjiris/jurisprudencia-document'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<PartialJurisprudenciaDocument[]>
+    req: NextApiRequest,
+    res: NextApiResponse<PartialJurisprudenciaDocument[]>
 ) {
-    let {proc, quuid} = req.query;
-    if( !proc || Array.isArray(proc)) throw new Error("Invalid request")
+    let { proc, uuid: quuid } = req.query;
+    if (!proc || Array.isArray(proc)) throw new Error("Invalid request")
     let uuid = Array.isArray(quuid) ? quuid[0] : quuid || "";
-    
-    let m = proc.match(/(?<base>[^/]+\/\w+\.\w+)(-\w+)?\./); 
-    if( !m ){
+
+    let m = proc.match(/(?<base>[^/]+\/\w+\.\w+)(-\w+)?\./);
+    if (!m) {
         return res.status(200).json([]);
     }
     const authed = await authenticatedHandler(req);
-    return await search({wildcard: {"Número de Processo": `${m.groups?.base}*`}}, {pre:[], after:[]}, 0, {}, 100, {_source: ['Número de Processo', "UUID", "Data"]}, authed).then( related => {
-        return related.hits.hits.map( hit => hit._source ? ({
+    return await search({ wildcard: { "Número de Processo": `${m.groups?.base}*` } }, { pre: [], after: [] }, 0, {}, 100, { _source: ['Número de Processo', "UUID", "Data"] }, authed).then(related => {
+        return related.hits.hits.map(hit => hit._source ? ({
             "Número de Processo": hit._source["Número de Processo"],
             UUID: hit._source.UUID,
             Data: hit._source.Data
-        }) : {}).filter( hit => hit.UUID && hit.UUID.indexOf(uuid) != 0);
-    }).then( l => res.status(200).json(l))
+        }) : {}).filter(hit => hit.UUID && hit.UUID.indexOf(uuid) != 0);
+    }).then(l => res.status(200).json(l))
 }
