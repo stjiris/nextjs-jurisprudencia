c8cdd9d7709111ddebfba8919b6c0c6827128989
Fábio Sobrinho <fabio.sobrinho@tecnico.ulisboa.pt>
2024-04-26 16:18:55 +0100

    crontab integration

--- src/pages/api/rss.ts
+++ src/pages/api/rss.ts
@@ -1,13 +1,24 @@
+import { aggs } from "@/core/elasticsearch";
 import { createReadStream } from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
 
 export default async function docApiHandler(
     req: NextApiRequest,
     res: NextApiResponse
 ){
-    
-    res.writeHead(200, {
-        "Content-Type": "text/xml"
-    })
-    createReadStream("rss.xml").pipe(res)
+    const aggKey = Array.isArray(req.query.area) ? req.query.area[0] : req.query.area || "rss";
+
+        const pathToRSS = path.join(process.env.RSS_FOLDER || "", aggKey + ".xml")
+        const stream = createReadStream(pathToRSS)
+        stream.on("error", function(){
+            res.status(404).end("Not found")
+        })
+        stream.on("ready", function(){
+            res.writeHead(200, {
+                "Content-Type": "text/xml"
+            })
+        })
+        stream.pipe(res)
+
 }
