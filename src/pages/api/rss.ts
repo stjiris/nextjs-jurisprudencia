99a9132f98415d4a604ecb678dd0cc93e1a157dc
Fábio Sobrinho <fabio.sobrinho@tecnico.ulisboa.pt>
2024-04-26 17:53:44 +0100

    made rss for specific areas

--- src/pages/api/rss.ts
+++ src/pages/api/rss.ts
@@ -8,17 +8,16 @@ export default async function docApiHandler(
     res: NextApiResponse
 ){
     const aggKey = Array.isArray(req.query.area) ? req.query.area[0] : req.query.area || "rss";
-
-        const pathToRSS = path.join(process.env.RSS_FOLDER || "", aggKey + ".xml")
-        const stream = createReadStream(pathToRSS)
-        stream.on("error", function(){
-            res.status(404).end("Not found")
+    const pathToRSS = path.join(process.env.RSS_FOLDER || "", aggKey + ".xml")
+    const stream = createReadStream(pathToRSS)
+    stream.on("error", function(){
+        res.status(404).end("Not found")
+    })
+    stream.on("ready", function(){
+        res.writeHead(200, {
+            "Content-Type": "text/xml"
         })
-        stream.on("ready", function(){
-            res.writeHead(200, {
-                "Content-Type": "text/xml"
-            })
-        })
-        stream.pipe(res)
+    })
+    stream.pipe(res)
 
 }
