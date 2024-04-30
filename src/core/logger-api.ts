68956f0568aaf06d33e61f081a8abf0f7a187b0c
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2024-04-30 19:46:26 +0100

    Fix logger start date

--- src/core/logger-api.ts
+++ src/core/logger-api.ts
@@ -2,8 +2,8 @@ import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
 
 
 export default function LoggerApi(cb: NextApiHandler): NextApiHandler{
-    const start = new Date();
     return async function(req: NextApiRequest, res: NextApiResponse){
+        const start = new Date();
         let r = await cb(req, res);
         const end = Date.now();
         console.log(`[API] ${start.toISOString()} ${req.method} ${req.url} ${res.statusCode} ${end - (+start)}ms`);
