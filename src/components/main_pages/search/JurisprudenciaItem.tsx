f61f5a24ce5fd9ebf2a98dfc554b6e808f9d5621
Miguel Fonseca <miguel.dias.fonseca@gmail.com>
2026-04-10 10:15:41 +0100

    changed summary

--- src/components/main_pages/search/JurisprudenciaItem.tsx
+++ src/components/main_pages/search/JurisprudenciaItem.tsx
@@ -87,19 +87,19 @@ function SumarioPreview({ sumario, sumarioName }: { sumario: string; sumarioName
 
     return (
         <div className="col-12 col-lg-8 rounded" style={{ backgroundColor: "var(--secondary-gold)", whiteSpace: "pre-line" }}>
-            {isTruncated && (
-                <button
-                    type="button"
-                    className="btn btn-link p-0 me-1 align-baseline"
-                    style={{ color: 'inherit', lineHeight: 1 }}
-                    onClick={() => setExpanded(e => !e)}
-                >
-                    <i className={`bi ${expanded ? 'bi-caret-down-fill' : 'bi-caret-right-fill'}`} />
-                </button>
-            )}
             <b>{sumarioName}:</b>{' '}
             {expanded ? cleanText : cleanText.substring(0, SUMARY_SIZE)}
             {!expanded && isTruncated && '...'}
+            {isTruncated && (
+                <button
+                    type="button"
+                    className="btn btn-link p-0 ms-1 align-baseline"
+                    style={{ color: 'inherit', lineHeight: 1 }}
+                    onClick={() => setExpanded(e => !e)}
+                >
+                    <i className={`bi ${expanded ? 'bi-caret-up-fill' : 'bi-caret-down-fill'}`} />
+                </button>
+            )}
         </div>
     );
 }
